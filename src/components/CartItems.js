0c636319ef2f69267245593aef251c5a88500494
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-05-19 00:11:00 -0400

    modificaciones al codigo 510 terminado

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { ChevronDown, ChevronUp } from '../icons';
 
-const CartItems = () => {
-  return (
-    <div>CartItems</div>
-  )
+const CartItems = ({id, img, price, title, amount}) => {
+  return <article className='cart-item'>
+    <img src={img} alt={title}/>
+    <div>
+        <h4>{title}</h4>
+        <h4 className='item-price'>${price}</h4>
+        <button className='remove-btn'>remove</button>
+    </div>
+    <div>
+      <button className='amount-btn'>
+          <ChevronUp />
+      </button>
+      <p className='amount'>{amount}</p>
+      <button className='amount-btn'>
+          <ChevronDown />
+      </button>
+    </div>
+  </article>
+  
 }
 
 export default CartItems;
