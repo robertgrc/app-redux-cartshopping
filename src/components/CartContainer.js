62ac2db36b0c8aaeaa241d3c0885d02679295c22
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-05-18 23:38:31 -0400

    Practicando Redux John smilga

---
+++ src/components/CartContainer.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import  CartItems  from './CartItems'
+
+const CartContainer = () => {
+
+  const {cartItems, total, amount} = useSelector((store)=>store.cart);
+
+  if (amount < 1){
+      return (
+       <section className='cart'>
+        <header>
+            <h2>your bag</h2>
+            <h4 className='empty-cart'>is currently empty</h4>
+        </header>
+       </section>
+      );
+  }
+  return (
+    <section className='cart'>
+        <header>
+            <h2>Your bag</h2>
+        </header>
+        <div>
+            {cartItems.map((item)=>{
+                return <CartItems key={item.id} {...item} />
+            })}
+        </div>
+        <footer>
+          <hr />
+          <div  className='cart-total'>
+            <h4>
+                total 
+                <span>${total}</span>
+            </h4>
+            <button className='btn clear-btn'>clear cart</button>
+          </div>
+        </footer>
+    </section>
+  )
+};
+
+export default CartContainer
